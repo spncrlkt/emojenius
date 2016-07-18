0c1c3b28f1b3ccda2626ac6b01c9125ba527f4f7
psliechty <spencerliechty@gmail.com>
2016-07-18 12:18:30 -0500

    Init

---
+++ src/containers/Butt.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>BUTT BUTT BUTT</h2>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+
+
+import { getAllProducts } from './actions'
+
+
+import { Router, Route, hashHistory } from 'react-router'
+
+import App from './containers/App'
+import Butt from './containers/Butt'
+
+const middleware = process.env.NODE_ENV === 'production' ?
+  [ thunk ] :
+  [ thunk ]
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(...middleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+store.dispatch(getAllProducts())
+
+render(
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="butt" component={Butt}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
