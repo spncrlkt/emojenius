e21cb0c2cbeb1f6bb58e3d51265da2d10b2c8455
psliechty <spencerliechty@gmail.com>
2016-07-20 19:59:25 -0500

    add search bar ui

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,26 +3,38 @@ import AppBar from 'material-ui/lib/app-bar';
 import IconButton from 'material-ui/lib/icon-button';
 import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
 import IconMenu from 'material-ui/lib/menus/icon-menu';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuIcon from 'material-ui/lib/svg-icons/navigation/menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
+import TextField from 'material-ui/lib/text-field';
+
+const searchFieldStyles = {
+  width: 200,
+  marginTop: 5,
+};
 
 const Navbar = () => (
   <AppBar
-    title="Title"
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    iconElementRight={
+    title="EMOJI"
+    iconElementLeft={
       <IconMenu
         iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
+          <IconButton><MenuIcon /></IconButton>
         }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        targetOrigin={{horizontal: 'left', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'left', vertical: 'top'}}
       >
         <MenuItem primaryText="Refresh" />
         <MenuItem primaryText="Help" />
         <MenuItem primaryText="Sign out" />
       </IconMenu>
     }
+    children= {[
+      <TextField
+        style={ searchFieldStyles }
+        key='search'
+        hintText="Search Emoji"
+      />
+    ]}
   />
 );
 
