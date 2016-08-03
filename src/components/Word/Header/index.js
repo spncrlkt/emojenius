f51cd1df94fa9187414c119051544e93698f647b
psliechty <spencerliechty@gmail.com>
2016-08-02 20:38:41 -0700

    WIP AddDefinition component

--- src/components/Word/Header/index.js
+++ src/components/Word/Header/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/lib/paper';
 
 import styles from './styles.css';
 
+import AddDefinition from './AddDefinition';
+
 export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleTouchTap = (e) => {
+    this.setState({open: !this.state.open});
+  }
+
   render() {
     const {
       params
@@ -10,8 +24,15 @@ export default class Header extends Component {
 
     return (
       <div className={ styles.container }>
-        <div className={ styles.inner }>
-          <h2>{ params.title }</h2>
+        <div
+          className={ styles.inner }
+          onTouchTap={ this.handleTouchTap }>
+          <Paper zDepth={ 2 }>
+            <div>
+              <h2>{ params.title }</h2>
+              { this.state.open && <AddDefinition /> }
+            </div>
+          </Paper>
         </div>
       </div>
     );
--- txt/devnotes.md
+++ txt/devnotes.md
@@ -3,6 +3,7 @@
 ## todo
 - [ ] admin add def as user
 - [ ] admin autofetch on add word/def - redux sagas
+- [ ] login redirect to current page
 - [ ] add def from word page
 - [ ] upvote/downvote action
 - [ ] search action
